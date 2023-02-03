d03d21f38164927290d7e552f5f24318f728b6b0
chengshi <645623005@qq.com>
2023-02-03 15:43:13 +0800

    leetcode17 18

---
+++ src/leetcode/leetcode18.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class leetcode18 {
+    public static void main(String[] args) {
+        leetcode18 leetcode18 = new leetcode18();
+        int[] nums = new int[]{1000000000,1000000000,1000000000,1000000000};
+        int target = -294967296;
+        System.out.println(leetcode18.fourSum(nums,target));
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> resList = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return resList;
+        }
+
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 3;i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            for(int j = i + 1; j < nums.length - 2;j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                    long res = (long)nums[i] + (long)nums[j] + (long)nums[left] + (long)nums[right];
+                    if(res == target){
+                        resList.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        }
+                        left++;
+                        while(left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        right--;
+                    }else if(res < target){
+                        left++;
+                    }else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return resList;
+    }
+
+}
+
