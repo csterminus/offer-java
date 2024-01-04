fd23be0a514c1a02fb73362c8753ec74a1b22479
chengshi <645623005@qq.com>
2024-01-04 18:38:14 +0800

    leetcode160

---
+++ src/leetcode/Leetcode160.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * @author chengshi
+ * @date 2024/1/4 18:36
+ */
+public class Leetcode160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while (pa != pb) {
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
+        }
+        return pa;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
