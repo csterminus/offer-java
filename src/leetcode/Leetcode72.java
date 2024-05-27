6e27c56c280120a3ac648f03554e7ed8baceadb0
chengshi <645623005@qq.com>
2024-05-27 23:26:36 +0800

    提交代码

--- src/leetcode/Leetcode72.java
+++ src/leetcode/Leetcode72.java
@@ -9,23 +9,34 @@ public class Leetcode72 {
 
     }
 
+    /**
+     * dp[i][j] = min(dp[i-1][j] + 1,    // 删除
+     *                dp[i][j-1] + 1,    // 插入
+     *                dp[i-1][j-1] + 1)  // 替换
+     *                初始化
+     * dp[0][j] 表示将空字符串转换成 word2 的前 j 个字符，需要 j 次插入操作，即 dp[0][j] = j。
+     * dp[i][0] 表示将 word1 的前 i 个字符转换成空字符串，需要 i 次删除操作，即 dp[i][0] = i。
+     * @param word1
+     * @param word2
+     * @return
+     */
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            dp[0][i] = i;
+        for(int i = 0;i <= m;i++){
+            dp[i][0] = i;
         }
-        for (int j = 0; j <= n; j++) {
-            dp[j][0] = j;
+        for(int j = 0;j <= n;j++){
+            dp[0][j] = j;
         }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+        for(int i = 1;i <= m;i++){
+            for(int j = 1;j <= n;j++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    int temp = Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1);
-                    dp[i][j] = Math.min(temp, dp[i - 1][j - 1] + 1);
+                }else{
+                    int temp = Math.min(dp[i - 1][j] + 1,dp[i][j - 1] + 1);
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1,temp);
                 }
             }
         }
